0be235d21ec473975071668dc33ba3cf67193e75
Gukss <dongkuk97@naver.com>
2022-05-08 13:54:02 +0900

    NavBar 확인

--- grise/src/App.js
+++ grise/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { FeedbackRequestPage } from './Pages/FeedbackRequestPage';
+// import { FeedbackRequestPage } from './Pages/FeedbackRequestPage';
+import NavBar from './Pages/NavBar'
 
 function App() {
   return (
     <div className="App">
-
+			<NavBar />
     </div>
   );
 }
