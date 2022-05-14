5ad70ddf17c2365373a8bc387087a7585ab1ba3c
Gukss <dongkuk97@naver.com>
2022-05-14 20:00:26 +0900

    메인페이지 버튼 생성 버튼 누르면 id따라서 밑줄 바뀌게

--- grise/src/Pages/MainPage/Main.jsx
+++ grise/src/Pages/MainPage/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from "../NavBar";
+import Header from "./Header";
 
 const Main = () => {
 
 	return (
     <>
       <NavBar />
+			<Header />
     </>
   );
 };
