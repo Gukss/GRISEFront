f9469bf8b95570c135638f31240fc4b8c07a53a0
SeungHyunH <vkflek1232@naver.com>
2022-05-16 19:26:43 +0900

    TutorList 자세히보기, css 구현

---
+++ grise/src/Pages/TuteeMainPage/List/TutorItem.jsx
@@ -0,0 +1,74 @@
+import React,{forwardRef,useRef} from 'react'
+import styled from 'styled-components';
+import { ReactComponent as Star} from '../../../Image/star.svg';
+import { ReactComponent as InfoImage} from '../../../Image/info.svg';
+const TutorItem = forwardRef((props,ref) => {
+    const detailDivRef = useRef();
+    const isShowDetail = useRef(false);
+    const showDetailInfo = () =>{
+        detailDivRef.current.style.display = (isShowDetail.current)? 'none':'block';
+        isShowDetail.current = !isShowDetail.current;
+        if(props.isEnd){detailDivRef.current.scrollIntoView({ behavior: 'smooth' });}
+    }
+    return (
+        <div ref={ref} style={{borderBottom:"#3A6C7B solid 0.2rem"}}>
+            <Container>
+                <div style={{float:'left'}}>
+                    <Title>{props.data?.tutor.name}</Title>
+                    <div>
+                        <StarImage><Star style={{width:'1.5rem',height:'1.5rem'}}></Star></StarImage>
+                        <Score>{props.data?.tutor.score}</Score>
+                    </div>
+                </div>
+                <InfoBtn onClick={showDetailInfo}><InfoImage></InfoImage></InfoBtn>
+            </Container>
+            <DetailInfo ref = {detailDivRef}>
+                {props.data?.tutor.resume.content}
+            </DetailInfo>
+        </div>
+    )
+});
+
+const InfoBtn = styled.span`
+    float: right;
+    margin-top: 0.8rem;
+`
+
+const StarImage = styled.span`
+    margin-left: 1.5rem;
+    margin-right: 0.5rem;
+`
+
+const Title = styled.div`
+    margin-left: 1.5rem;
+    margin-top: 0.5rem;
+    display: block;
+    font-size: 1rem;
+    text-align:left;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 900;
+    color: #3A6C7B;
+    height: 1rem;
+`
+
+const Score = styled.span`
+    color: #000000;
+    font-size: 0.9rem;
+    vertical-align: 0.4rem;
+`
+
+const Container = styled.div`
+    width: 100%;
+    height: 4rem;
+    color: white;
+    font-weight: 900;
+    font-size: 2rem;
+    flex: 0 0 auto;
+`
+const DetailInfo = styled.div`
+    display: none;
+    margin-left: 1.5rem;
+    text-indent : -2.5rem;
+`
+export default TutorItem;
