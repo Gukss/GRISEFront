1068df6e8ea5de3dc7d22c0a561258dc5b3c39d6
Gukss <dongkuk97@naver.com>
2022-05-17 20:34:02 +0900

    oauth 요청 코드 추가 netlify 라우팅 위해서 _redirect 추가

--- grise/src/Pages/LoginPage/TuteeLoginPage.jsx
+++ grise/src/Pages/LoginPage/TuteeLoginPage.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 import {ReactComponent as TuteeLogo} from '../../Image/TuteeLogo.svg';
 import GoogleBtn from '../../Image/btn_google_signin_light_normal_web.png';
+import axios from 'axios';
+
+
 export const TuteeLoginPage = () => {
+	const onClick = () => {
+		axios
+      .get("/oauth2/authorization/google?redirect_uri=http://localhost:3000/oauth/redirect")
+      .then((response) => {
+        console.log(response);
+      });
+	}
   return (
     <Wrap>
-      <TuteeLogo style={{width:'100%',height:'100%'}}></TuteeLogo>
-      <div
-        onClick={()=>{console.log('구글로그인')}}
-        style={{textAlign:'center'}}
-      >
+      <TuteeLogo style={{ width: "100%", height: "100%" }}></TuteeLogo>
+      <div onClick={onClick} style={{ textAlign: "center" }}>
         <img src={GoogleBtn} alt="구글 로그인" />
       </div>
     </Wrap>
-  )
+  );
 }
 
 const Wrap = styled.div`
