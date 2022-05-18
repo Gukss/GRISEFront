952b310a91dc1d08e843d6389f8e9a5503f26c8f
SeungHyunH <77517470+SeungHyunH@users.noreply.github.com>
2022-05-18 14:55:34 +0900

    sideBar 수정중

--- grise/src/Pages/NavBar.jsx
+++ grise/src/Pages/NavBar.jsx
@@ -1,15 +1,21 @@
-import React,{useState} from 'react';
+import React,{useState,useRef,useEffect} from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import { ReactComponent as Search} from '../Image/search.svg';
 import { ReactComponent as Menu} from '../Image/Menu.svg';
 import { ReactComponent as Logo} from '../Image/GRISE_logo.svg';
+import SideBarContainer from './SideBar/SideBarContainer';
 
 export const NavBar = () => {
   const [searchText, setSearchText] = useState('');
   const [hiddenSearch,setHiddenSearch] = useState(true);
-  const [hiddenMenu,setHiddenMenu] = useState(true);
-  
+  const sideBarRef = useRef();
+  const onClickMenu = () =>{
+    sideBarRef.current.style.display='block';
+  }
+  useEffect(()=>{
+    sideBarRef.current.style.display='none';
+  },[]);
   return (
     <div>
       <div
@@ -34,23 +40,16 @@ export const NavBar = () => {
   						marginLeft: "0.5rem"}}/>
             {/* <Home>GRISE</Home> */}
           </Link>
-          <SlideMenu hidden={hiddenMenu}>
-            <Menu
-              style={{ width: "2rem", height: "2rem", float: "right" }}
-              onClick={() => {
-                setHiddenMenu(!hiddenMenu);
-              }}
-            />
-          </SlideMenu>
+          <SideBar ref={sideBarRef}>
+            <SideBarContainer/>
+          </SideBar>
           <Menu
             style={{
               width: "2rem",
               height: "2rem",
               float: "right",
             }}
-            onClick={() => {
-              setHiddenMenu(!hiddenMenu);
-            }}
+            onClick={onClickMenu}
           ></Menu>
           <Search
             style={{
@@ -81,33 +80,20 @@ export const NavBar = () => {
   );
 }
 
-// const Home = styled.div`
-//   color: #3A6C7B;
-//   font-family: 'Lora';
-//   font-style: normal;
-//   font-weight: 400;
-//   font-size: 2rem;
-//   line-height:2rem;
-//   text-align: center;
-//   width: 2rem;
-//   height: 2rem;
-//   float: left;
-//   margin-left: 0.5rem;
-// `
+const SideBar = styled.div`
+  position:absolute;
+  width:100%;
+  height:100%;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+`
 
 const Input = styled.input`
   display:${props=>props.hidden ? 'none':'block'};
   width:10rem;
   height:3rem;
   float:right;
-`
-const SlideMenu = styled.div`
-  display:${props=>props.hidden ? 'none':'block'};
-  width:40%;
-  height:100%;
-  left:60%;
-  position:absolute;
-  background-color: gray;
-  transition:1s;
 `
 export default NavBar;
---
+++ grise/src/Pages/SideBar/SideBarItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideBarItem = () => {
+  return (
+    <div>SideBarItem</div>
+  )
+}
+
+export default SideBarItem
