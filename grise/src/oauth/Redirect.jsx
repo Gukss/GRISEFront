99d41cadeedae380ef9bdcdda66d26975def56d6
Gukss <dongkuk97@naver.com>
2022-05-31 22:59:34 +0900

    1. useNavigate import 수정

--- grise/src/oauth/Redirect.jsx
+++ grise/src/oauth/Redirect.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useNavigate } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Redirect = (props) => {
